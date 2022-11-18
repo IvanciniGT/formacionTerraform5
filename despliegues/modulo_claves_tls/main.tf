630b1f11d1aed13cd2007e5beb048a7290004f48
Ubuntu <ubuntu@ip-172-31-20-70.eu-west-1.compute.internal>
2022-11-18 16:03:04 +0000

    dia4

--- despliegues/modulo_claves_tls/main.tf
+++ despliegues/modulo_claves_tls/main.tf
@@ -1,29 +1,29 @@
 
 locals {
-    directorio_ficheros = var.file_destination # Le añadiré una barra al final, si no la tiene
+    directorio_ficheros = endswith(var.file_destination,"/") ? var.file_destination : "${var.file_destination}/"
+                         # Lleva barra al final              ? Pues lo dejo como está    Que no? Se la pongo
 
-    existe_fichero_clave_privada_pem        = true
-    existe_fichero_clave_privada_open_ssh   = false
-    existe_fichero_clave_publica_pem        = 
-    existe_fichero_clave_publica_open_ssh   = 
+    existe_fichero_clave_privada_pem        = fileexists( "${locals.directorio_ficheros}privateKey.pem" )
+    existe_fichero_clave_privada_open_ssh   = fileexists( "${locals.directorio_ficheros}privateKey.openssh" ) 
+    existe_fichero_clave_publica_pem        = fileexists( "${locals.directorio_ficheros}publicKey.pem" ) 
+    existe_fichero_clave_publica_open_ssh   = fileexists( "${locals.directorio_ficheros}publicKey.openssh" ) 
     
-    contendo_fichero_clave_privada_pem      = "texto"
-    contendo_fichero_clave_privada_open_ssh = 
-    contendo_fichero_clave_publica_pem      = 
-    contendo_fichero_clave_publica_open_ssh = 
-    
-    
-    existen_todos_los_ficheros_claves       =   existe_fichero_clave_privada_pem        &&
+    existen_todos_los_ficheros_claves       = ( existe_fichero_clave_privada_pem        &&
                                                 existe_fichero_clave_privada_open_ssh   &&
                                                 existe_fichero_clave_publica_pem        &&
-                                                existe_fichero_clave_publica_open_ssh
+                                                existe_fichero_clave_publica_open_ssh )
+                                                
+    contendo_fichero_clave_privada_pem      = existe_fichero_clave_privada_pem ? file( "${locals.directorio_ficheros}privateKey.pem" )      : null
+    contendo_fichero_clave_privada_open_ssh = existe_fichero_clave_privada_pem ? file( "${locals.directorio_ficheros}privateKey.openssh" )  : null
+    contendo_fichero_clave_publica_pem      = existe_fichero_clave_privada_pem ? file( "${locals.directorio_ficheros}publicKey.pem" )       : null
+    contendo_fichero_clave_publica_open_ssh = existe_fichero_clave_privada_pem ? file( "${locals.directorio_ficheros}publicKey.openssh" )   : null
 }
 
 # Esto lo estariamos generando siempre.... no se si es lo que queremos.
 resource "tls_private_key" "claves" {
     # Me permitira generar o no el recurso: 0 o 1
     count       = locals.existen_todos_los_ficheros_claves && ! force_recreate ? 0 : 1
-                  # Si existen ficheros anteriores de claves y no se feurza el recreado
+                  # Si existen ficheros anteriores de claves y no se fuerza el recreado
 
     algorithm   = var.algoritmo.nombre
     ecdsa_curve = var.algoritmo.nombre == "ECDSA" ? var.algoritmo.configuracion : null
@@ -32,10 +32,10 @@ resource "tls_private_key" "claves" {
     provisioner "local-exec" {
         command = <<EOT
                         mkdir -p ${locals.directorio_ficheros}
-                        echo '${self.private_key_pem}'       > ${locals.directorio_ficheros}privateKey.pem
-                        echo '${self.private_key_openssh}'   > ${locals.directorio_ficheros}privateKey.openssh
-                        echo '${self.public_key_pem}'        > ${locals.directorio_ficheros}publicKey.pem
-                        echo '${self.public_key_openssh}'    > ${locals.directorio_ficheros}privateKey.openssh
+                        echo '${self.private_key_pem}'       > "${locals.directorio_ficheros}privateKey.pem"
+                        echo '${self.private_key_openssh}'   > "${locals.directorio_ficheros}privateKey.openssh"
+                        echo '${self.public_key_pem}'        > "${locals.directorio_ficheros}publicKey.pem"
+                        echo '${self.public_key_openssh}'    > "${locals.directorio_ficheros}publicKey.openssh"
                     EOT
     }
 }
